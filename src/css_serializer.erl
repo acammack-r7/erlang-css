57e016b750e2e1ba94d03c5a1919f5b062baeed3
Adam Cammack <adam_cammack@rapid7.com>
2016-12-05 11:54:47 -0600

    Fix round-tripping with unrestricted hashes

--- src/css_serializer.erl
+++ src/css_serializer.erl
@@ -22,7 +22,8 @@ serialize(Token) when is_tuple(Token) ->
     {function, Ident} -> <<(css_escape(Ident))/binary, $(>>;
     {function, Ident, Body} -> <<(css_escape(Ident))/binary, $(, (serialize(Body))/binary, $)>>;
     {url, URL} -> <<"url(", (css_escape(URL))/binary, $)>>;
-    {{hash, _ID}, Ident} -> <<$#, (css_escape(Ident))/binary>>;
+    {{hash, id}, Ident} -> <<$#, (css_escape(Ident))/binary>>;
+    {{hash, unrestricted}, Val} -> <<$#, (css_escape(Val, unrestricted))/binary>>;
     {{dimension, _Integer}, {_, Number, Ident}} -> <<Number/binary, (css_escape(Ident))/binary>>;
     {{number, _Integer}, {_, Number}} -> <<Number/binary>>;
     {delim, Char} -> <<Char/utf8>>;
@@ -54,13 +55,21 @@ serialize(Token) when is_atom(Token) ->
 
 -spec css_escape(Name::binary()) -> Escaped::binary().
 css_escape(Unescaped) ->
-  << <<(css_escape_char(Char))/binary>> || <<Char/utf8>> <= Unescaped >>.
+  css_escape(Unescaped, name).
 
--spec css_escape_char(Character::integer()) -> Escaped::binary().
+-spec css_escape(Part::binary(), Type::atom()) -> Escaped::binary().
+css_escape(Unescaped, Type) ->
+  << <<(css_escape_char(Char, Type))/binary>> || <<Char/utf8>> <= Unescaped >>.
+
+-spec css_escape_char(Character::integer(), Type::atom()) -> Escaped::binary().
 % Characters that always start an identifier never have to be escaped
-css_escape_char(Char) when ?name_start(Char) ->
+css_escape_char(Char, _Type) when ?name_start(Char) ->
   <<Char/utf8>>;
-css_escape_char(Char) ->
+% Unrestricted hashes need less escaping
+css_escape_char(Char, unrestricted) when ?is_name(Char) ->
+  <<Char/utf8>>;
+% Escape the rest
+css_escape_char(Char, _Type) ->
   <<$\\, (integer_to_binary(Char, 16))/binary, 32>>.
 
 -spec serialize_unicode_range(Begin::integer(), End::integer()) -> Range::binary().
